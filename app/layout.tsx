f705ab27beaeb4762d6446a377e8893cd46d03f6
Vladislav Vanli <onevlade44@gmail.com>
2026-02-05 14:25:04 -0800

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,6 +135,22 @@ export default function RootLayout({
             gtag('config', 'AW-17933281806');
           `}
         </Script>
+
+      {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vcu0xd6lbz");
+          `}
+        </Script>
+
+        
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* Structured data (helps Google understand what the page is) */}
